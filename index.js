2a5952d03610ac779462cd01a21080dd9c1abab3
Nazia R <nazia.huma@gmail.com>
2021-05-10 02:34:14 +0100

    cleaned up the code and removed todo comments

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-// TODO: Include packages needed for this application
-
 const inquirer = require("inquirer");
 const createFile = require("./utils/createFile");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
+
 const questions = [
   {
     type: "input",
@@ -54,12 +52,10 @@ const questions = [
   },
 ];
 
-// TODO: Create a function to write README file
 const writeToFile = (fileName, data) => {
     
 };
 
-// TODO: Create a function to initialize app
 const init = async () => {
 
     const answers = await inquirer.prompt(questions)
@@ -69,5 +65,4 @@ const init = async () => {
     createFile(markdown)
 };
 
-// Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-const renderLicenseBadge = (license) => {};
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-const renderLicenseLink = (license) => {};
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-const renderLicenseSection = (license) => {};
-
-// TODO: Create a function to generate markdown for README
 const generateMarkdown = (answers) => {
   const {
     titleName,
